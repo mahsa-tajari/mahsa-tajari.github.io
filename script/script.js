3a918c43a35519a7448d3f996c64fedacc87695d
Mahsa <108124183+mahsa-tajari@users.noreply.github.com>
2023-01-15 10:35:40 +0330

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,42 @@
+const $ = document 
+const menuItemElm = $.querySelectorAll('.menu__item')
+const menuClose = $.querySelector('.menu__close')
+const navMenu = $.querySelector('.nav-menu')
+const navIcon = $.querySelector('.nav-icon')
+let isMenuOpen = false
+let navIconImgElm = $.querySelector('.nav-icon__img')
+
+function openingOptionsHandler (icon){
+    let collapsedMenuElm = icon.firstElementChild.nextElementSibling.nextElementSibling;
+    let arrowElm = icon.firstElementChild.nextElementSibling
+    let style = window.getComputedStyle(collapsedMenuElm)
+
+    if(style.display === 'none'){
+        collapsedMenuElm.style.display = 'flex'
+        arrowElm.src = 'file:///E:/project/project2/images/icon-arrow-up.svg'
+    }
+    else{
+        collapsedMenuElm.style.display = 'none'
+        arrowElm.src = 'file:///E:/project/project2/images/icon-arrow-down.svg'
+    }
+}
+
+function openingMenuHandler(){
+    if(!isMenuOpen){
+        navMenu.classList.add('nav-menu--open')
+        navIconImgElm.src = 'file:///E:/project/project2/images/icon-close-menu.svg'
+        isMenuOpen = true
+    }
+    else {
+        navMenu.classList.remove('nav-menu--open')
+        navIconImgElm.src = 'file:///E:/project/project2/images/icon-menu.svg'
+        isMenuOpen = false
+    }
+}
+
+menuItemElm.forEach(element => {
+    element.addEventListener('tochend',function(){
+        openingOptionsHandler(element)
+    })
+});
+navIcon.addEventListener('touchend',openingMenuHandler)
